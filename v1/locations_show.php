8b5451a8ba67d6bae12ca5f57d2aebc0a04b0f78
Charles Harvey <harvey.charles@gmail.com>
2021-01-11 11:00:19 +0000

    add acf fields to location

--- v1/locations_show.php
+++ v1/locations_show.php
@@ -5,6 +5,13 @@ $location_id =  intval($_GET['location_id']);
 $post = get_post($location_id);
 
 
+
+$post->description = get_field('description', $post->ID);
+$post->responsible = get_field('responsible', $post->ID);
+
+
+
+
 // remove unncessary params
 $unncessary_params = ['comment_count', 'post_status', 'post_mime_type',  'ping_status', 'comment_status', 'post_parent', 'post_date_gmt',  'post_modified_gmt', 'post_password',  'post_excerpt', 'pinged', 'to_ping', 'filter', 'post_content_filtered'];
 foreach ($unncessary_params as $up) {
